8908aa0ef4b6e679e1767207de3334358eda278f
Ivo D'Ursi <ivodursi211@gmail.com>
2023-09-22 14:29:26 -0300

    Ejercicios convertidos en hechos

---
+++ db.pl
@@ -0,0 +1,68 @@
+personalizar(1, 'No personalizada').
+personalizar(2, 'Personalizada').
+
+objetivo(1, hipertrofia).
+objetivo(2, fuerza).
+
+ubicacion(1, casa).
+ubicacion(2, gimnasio).
+
+experiencia(1, 'Sin experiencia').
+experiencia(2, 'Ejercicio esporádico').
+experiencia(3, 'Ejercicio regular').
+
+ejercicio_musculo('Escapulares', espalda).
+ejercicio_musculo('Dorsales en polea', espalda).
+ejercicio_musculo('Remo Parado', espalda).
+ejercicio_musculo('Press Plano', pecho).
+ejercicio_musculo('Press inclinado', pecho).
+ejercicio_musculo('Press TRX', pecho).
+ejercicio_musculo('Press militar', hombros).
+ejercicio_musculo('vuelos laterales', hombros).
+ejercicio_musculo('Press con Barra', hombros).
+ejercicio_musculo('Prensa de piernas inclinadas', isquios).
+ejercicio_musculo('Peso muerto rumano', isquios).
+ejercicio_musculo('Flexiones de caderas en máquinas', isquios).
+ejercicio_musculo('Curl con Barra w', biceps).
+ejercicio_musculo('Curl con mancuernas', biceps).
+ejercicio_musculo('Curl con polea alta', biceps).
+ejercicio_musculo('Sentadilla', cuadriceps).
+ejercicio_musculo('Prensa de piernas', cuadriceps).
+ejercicio_musculo('Sentadilla Búlgara', cuadriceps).
+ejercicio_musculo('Elevaciones de Talones Unilaterales', gemelos).
+ejercicio_musculo('Salto Tijera', gemelos).
+ejercicio_musculo('Elevaciones con talones en máquina smith', gemelos).
+ejercicio_musculo('Sentadilla sumo', aductores).
+ejercicio_musculo('Estocadas laterales', aductores).
+ejercicio_musculo('Sentadilla con bola suiza', aductores).
+ejercicio_musculo('Extensiones de muñeca con banda elástica', antebrazo).
+ejercicio_musculo('Curl de muñeca con barra', antebrazo ).
+ejercicio_musculo('Flexiones de muñeca con cuerda en polea alta', antebrazo).
+ejercicio_musculo('Shrugs en máquina Smith', trapecio).
+ejercicio_musculo('Encogimiento en Barra T', trapecio).
+ejercicio_musculo('Encogimiento con Mancuernas', trapecio).
+ejercicio_musculo('Elevaciones con piernas elevadas', abdominales).
+ejercicio_musculo('Elevaciones de piernas en suspensión', abdominales).
+ejercicio_musculo('Giros rusos', abdominales).
+ejercicio_musculo('Superman', lumbares).
+ejercicio_musculo('Hiperextensiones en el suelo', lumbares).
+ejercicio_musculo('Plancha inversa', lumbares).
+ejercicio_musculo('Hiperextensiones en máquinas', lumbares).
+
+
+% Entrada
+% Personalizada || No personalizada
+% Hipertrofia || Fuerza
+% En Casa || Gym
+% Frecuencia.. en hora por dia(entre 1 y 3)
+
+% Salida
+% Ejercicio 1
+% Ejercicio 2
+% ....
+% Ejercio N
+%   Primer Semana   Segunda Semana   Tercer Semana .. Quinta Semana
+%    Series x Rep   Series x Reps    Series x Reps   Series x rep
+
+
+
